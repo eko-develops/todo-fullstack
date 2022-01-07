914f79b085ee3841a07539ad3aede2a5033de0ea
eko-develops <ekodevelops@gmail.com>
2022-01-07 15:49:32 -0800

    Make connection to database with TypeORM

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import {createConnection} from "typeorm";
+
+(async () => {
+
+    const connection = await createConnection({
+        type: "postgres",
+        host: "localhost",
+        username: "postgres",
+        password: "123123",
+        database: "todo_fullstack",
+        entities: []
+    });
+
+    console.log('hello world');
+
+})();
