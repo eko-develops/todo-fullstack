c96061d28426996872bacb6344f51906a6f56537
eko-develops <ekodevelops@gmail.com>
2022-01-10 14:53:44 -0800

    Remove useless stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Helo world</h1>
+      <p>
+        Qui esse ut non deserunt officia ad eu aute incididunt. Magna minim eu
+        ullamco adipisicing est aliquip. Esse commodo qui consequat magna aute
+        esse aliquip elit veniam ad laborum occaecat ut mollit. Deserunt anim
+        cupidatat et in tempor nisi occaecat magna ea eu nulla anim cupidatat
+        dolor. Quis reprehenderit adipisicing aliqua excepteur consectetur id do
+        consequat veniam qui ea non. Ea deserunt dolor proident enim culpa
+        excepteur non excepteur mollit dolore labore. Nulla pariatur aliquip ex
+        dolor ipsum duis incididunt. Minim ut occaecat aute magna dolore mollit
+        dolore exercitation id est sit. Eiusmod tempor mollit ipsum sit et
+        fugiat officia cillum velit ea aliquip culpa. Cupidatat nulla minim non
+        proident officia fugiat eu.
+      </p>
     </div>
   );
 }
