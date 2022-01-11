2cc48370fe998362ab5c4e09831a7621a9dbe32c
eko-develops <ekodevelops@gmail.com>
2022-01-10 16:26:04 -0800

    Add tailwindcss to react project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Helo world</h1>
-      <p>
-        Qui esse ut non deserunt officia ad eu aute incididunt. Magna minim eu
-        ullamco adipisicing est aliquip. Esse commodo qui consequat magna aute
-        esse aliquip elit veniam ad laborum occaecat ut mollit. Deserunt anim
-        cupidatat et in tempor nisi occaecat magna ea eu nulla anim cupidatat
-        dolor. Quis reprehenderit adipisicing aliqua excepteur consectetur id do
-        consequat veniam qui ea non. Ea deserunt dolor proident enim culpa
-        excepteur non excepteur mollit dolore labore. Nulla pariatur aliquip ex
-        dolor ipsum duis incididunt. Minim ut occaecat aute magna dolore mollit
-        dolore exercitation id est sit. Eiusmod tempor mollit ipsum sit et
-        fugiat officia cillum velit ea aliquip culpa. Cupidatat nulla minim non
-        proident officia fugiat eu.
-      </p>
+      <h1 className="text-center">hello world</h1>
     </div>
   );
 }
