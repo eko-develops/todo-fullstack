9d3e1f88f0e0cc6b983f3bdf22809c0a6304fccc
eko-develops <ekodevelops@gmail.com>
2022-01-11 11:02:10 -0800

    Add sidebar component

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,15 @@
+interface Props {}
+
+const Sidebar = (props: Props) => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-32 m-0 bg-gray-900 text-white flex flex-col shadow-lg">
+      <i>icon</i>
+      <i>icon</i>
+      <i>icon</i>
+      <i>icon</i>
+      <i>icon</i>
+    </div>
+  );
+};
+
+export default Sidebar;
