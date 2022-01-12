8fea628a938a2f6dc5c4b5ee7ef663f4d0e94984
eko-develops <ekodevelops@gmail.com>
2022-01-12 15:26:33 -0800

    integrate graphql code generator

---
+++ client/src/generated/graphql.tsx
@@ -0,0 +1,162 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The javascript `Date` as string. Type represents date and time as the ISO Date string. */
+  DateTime: any;
+};
+
+export type CreateTodoInput = {
+  authorId: Scalars['ID'];
+  message?: InputMaybe<Scalars['String']>;
+  priority: Scalars['String'];
+  title: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTodo: Todo;
+  createUser: Scalars['Boolean'];
+  deleteTodo: Scalars['Boolean'];
+  deleteUser: Scalars['Boolean'];
+  updateTodo: Scalars['Boolean'];
+  updateUser: Scalars['Boolean'];
+};
+
+
+export type MutationCreateTodoArgs = {
+  options: CreateTodoInput;
+};
+
+
+export type MutationCreateUserArgs = {
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+};
+
+
+export type MutationDeleteTodoArgs = {
+  todoId: Scalars['ID'];
+};
+
+
+export type MutationDeleteUserArgs = {
+  userId: Scalars['ID'];
+};
+
+
+export type MutationUpdateTodoArgs = {
+  options: UpdateTodoInput;
+  todoId: Scalars['ID'];
+};
+
+
+export type MutationUpdateUserArgs = {
+  options: UpdateUserInput;
+  userId: Scalars['ID'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  todo?: Maybe<Todo>;
+  todos: Array<Todo>;
+  user?: Maybe<User>;
+  users: Array<User>;
+};
+
+
+export type QueryTodoArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryUserArgs = {
+  userId: Scalars['ID'];
+};
+
+export type Todo = {
+  __typename?: 'Todo';
+  authorId: Scalars['ID'];
+  createdAt: Scalars['DateTime'];
+  id: Scalars['ID'];
+  message?: Maybe<Scalars['String']>;
+  priority: Scalars['String'];
+  title: Scalars['String'];
+  updatedAt: Scalars['DateTime'];
+};
+
+export type UpdateTodoInput = {
+  message?: InputMaybe<Scalars['String']>;
+  title?: InputMaybe<Scalars['String']>;
+};
+
+export type UpdateUserInput = {
+  firstName?: InputMaybe<Scalars['String']>;
+  lastName?: InputMaybe<Scalars['String']>;
+};
+
+export type User = {
+  __typename?: 'User';
+  createdAt: Scalars['DateTime'];
+  firstName: Scalars['String'];
+  id: Scalars['ID'];
+  lastName: Scalars['String'];
+  todos: Array<Todo>;
+  updatedAt: Scalars['DateTime'];
+};
+
+export type TodosQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type TodosQuery = { __typename?: 'Query', todos: Array<{ __typename?: 'Todo', id: string, title: string, message?: string | null | undefined, priority: string, authorId: string }> };
+
+
+export const TodosDocument = gql`
+    query Todos {
+  todos {
+    id
+    title
+    message
+    priority
+    authorId
+  }
+}
+    `;
+
+/**
+ * __useTodosQuery__
+ *
+ * To run a query within a React component, call `useTodosQuery` and pass it any options that fit your needs.
+ * When your component renders, `useTodosQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useTodosQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useTodosQuery(baseOptions?: Apollo.QueryHookOptions<TodosQuery, TodosQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<TodosQuery, TodosQueryVariables>(TodosDocument, options);
+      }
+export function useTodosLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<TodosQuery, TodosQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<TodosQuery, TodosQueryVariables>(TodosDocument, options);
+        }
+export type TodosQueryHookResult = ReturnType<typeof useTodosQuery>;
+export type TodosLazyQueryHookResult = ReturnType<typeof useTodosLazyQuery>;
+export type TodosQueryResult = Apollo.QueryResult<TodosQuery, TodosQueryVariables>;
