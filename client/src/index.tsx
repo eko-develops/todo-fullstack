597f6c2a56028fe35960e3603c5e7381e4597514
eko-develops <ekodevelops@gmail.com>
2022-01-12 14:39:14 -0800

    Remove test query in index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,40 +4,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
-
-client
-  .query({
-    query: gql`
-      query Users {
-        users {
-          id
-          firstName
-          lastName
-          todos {
-            id
-            title
-            message
-            priority
-            authorId
-          }
-        }
-      }
-    `,
-  })
-  .then((data) => {
-    console.log(data);
-  });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
