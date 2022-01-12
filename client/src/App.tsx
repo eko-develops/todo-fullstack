8c08937ecd2cd787f208430b2ced4272655b318c
ekodevelops <ekodevelops@gmail.com>
2022-01-11 20:14:28 -0800

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,11 +7,11 @@ const App: React.FC = () => {
   return (
     <div className="w-full h-screen font-poppins bg-gray-800 flex text-white">
       <Sidebar />
-      <div className="bg-gray-900 w-5/6 h-5/6 m-auto rounded-lg shadow-xl">
-        <div className="p-8">
+      <main className="bg-gray-900 w-5/6 h-5/6 m-auto rounded-lg shadow-xl">
+        <div className="p-8 w-full bg-[red]">
           <Home />
         </div>
-      </div>
+      </main>
     </div>
   );
 };
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 interface Props {}
 
-const Home: React.FC = (props: Props) => {
+const Home = (props: Props) => {
   return (
-    <div>
-      <h1 className="font-bold text-4xl">Good evening, User</h1>
+    <div className="flex flex-col">
+      <h1 className="font-bold text-4xl mb-12">good evening, user</h1>
+      <div className="flex">
+        <div>
+          <h2 className="">todos</h2>
+        </div>
+      </div>
     </div>
   );
 };
